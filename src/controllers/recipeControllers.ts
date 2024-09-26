7b222aea76ead7c0aa8578492f44d7c4f4470b50
KRoses96 <45322919+KRoses96@users.noreply.github.com>
2024-09-26 13:39:33 +0100

    feat(endpoints): new add recipe endpoint

---
+++ src/controllers/recipeControllers.ts
@@ -0,0 +1,17 @@
+import recipeMethods from "../models/recipeMethods";
+import type { Request, Response } from "express";
+
+class RecipeControllers {
+	addRecipe = async (req: Request, res: Response): Promise<void> => {
+		try {
+			const { link } = req.body;
+			const createdRecipe = await recipeMethods.insertRecipe(link);
+			res.status(201).send(createdRecipe);
+		} catch (err) {
+			console.error(err);
+			res.status(500).send("Could not create Recipe");
+		}
+	};
+}
+
+export default new RecipeControllers();
--- src/models/recipeMethods.ts
+++ src/models/recipeMethods.ts
@@ -1,8 +1,49 @@
+import { db } from "../server";
+import type { RecipeType } from "../types";
+import { RecipeSchema } from "../types";
+import { recipe } from "./schema";
+
 class RecipeMethods {
-  
+	async insertRecipe(link: string): Promise<RecipeType | null> {
+		try {
+			let url: string | undefined;
+			if (link.includes("www.instagram.com")) {
+				url = process.env.INSTAGRAM_URL;
+			} else if (link.includes("www.youtube.com")) {
+				url = process.env.YOUTUBE_URL;
+			} else if (link.includes("www.tiktok.com")) {
+				url = process.env.TIKTOK_URL;
+			} else {
+				url = process.env.GENERAL_URL;
+			}
 
+			if (!url) {
+				console.error("No URL specified in the .env");
+			}
 
+			const response = await fetch(url as string, {
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+
+			if (!response.ok) {
+				console.error(`HTTP error! status: ${response.status}`);
+			}
+
+			const recipeData = response.json();
+			const validatedRecipe = RecipeSchema.parse(recipeData);
+			const createdRecipe = await db
+				.insert(recipe)
+				.values(validatedRecipe)
+				.returning();
+
+			return createdRecipe[0];
+		} catch (error) {
+			console.error(error);
+			return null;
+		}
+	}
 }
 
-
-export default new RecipeMethods()
+export default new RecipeMethods();
